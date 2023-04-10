262a8d6e5321957602b8b7238d591ebbe7c449d9
NefiTS <nefisantanna@gmail.com.br>
2023-04-10 10:57:36 -0300

    Iniciando

---
+++ MeuArquivo.py
@@ -0,0 +1,16 @@
+import pandas as pd # o as é um alias para o nome pandas, a mesma biblioteca tem uma interação com o Excel
+
+#importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+print(tabela_vendas)
+#visualizar a base de dados
+
+#faturamento por loja
+
+# quantidade de produtos vendidos por loja
+
+#ticket médio por produto em cada loja
+
+#enviar um e-mail com o relatório
+
